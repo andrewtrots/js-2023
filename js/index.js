6b7228b4a69122693635efb31ed802849b5b82d8
Andrew <andrewtrots@gmail.com>
2023-03-13 00:04:49 +0200

    add function from codewars

--- js/index.js
+++ js/index.js
@@ -23,3 +23,37 @@ function add(a, b) {
   const result = a + b;
   return result;
 }
+
+//Normal
+function describeAge(age) {
+  if (age <= 12) {
+    return "You're a(n) kid";
+  } else if (age >= 13 && age <= 17) {
+    return "You're a(n) teenager";
+  } else if (age >= 18 && age <= 64) {
+    return "You're a(n) adult";
+  } else {
+    return "You're a(n) elderly";
+  }
+}
+
+Conditional (ternary) operator
+function describeAge(n) {
+  return "You're a(n) " + (n <= 12
+    ? 'kid'
+    : n <= 17
+    ? 'teenager'
+    : n <= 64
+    ? 'adult'
+    : 'elderly');
+}
+
+/*8Arrow functrion + Conditional (ternary) operator
+const describeAge = (age) =>
+  age <= 12
+    ? "You're a(n) kid"
+    : age >= 13 && age <= 17
+    ? "You're a(n) teenager"
+    : age >= 18 && age <= 64
+    ? "You're a(n) adult"
+    : "You're a(n) elderly"; */
